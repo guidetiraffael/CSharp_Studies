3e01a2225c192c8d7273bf2b62ed33f64752d535
guidetiraffael <raffaelguideti@gmail.com>
2026-04-29 15:47:38 -0300

    Primeiro passo: Lógica básica de saldo URBS

---
+++ MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine("---Sistema de Transporte URBS (Protótipo)---");
+
+string nomeUsuario = "Raffael";
+double saldo = 2.00;
+double valorPassagem = 6.00;
+
+Console.WriteLine($"Usuário: {nomeUsuario}");
+Console.WriteLine($"Saldo Atual: R$ {saldo:F2}");
+
+if (saldo >= valorPassagem)
+{
+    saldo -= valorPassagem; 
+    Console.WriteLine("✅ Acesso Liberado! Boa viagem.");
+    Console.WriteLine($"Novo saldo: R$ {saldo:F2}");
+}
+else
+{
+    Console.WriteLine("❌ Saldo Insuficiente. Por favor, recarregue seu cartão.");
+}
