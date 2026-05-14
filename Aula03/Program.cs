16db44b63e28f01d33f4b686a74bbaf1f3593c65
guidetiraffael <raffaelguideti@gmail.com>
2026-05-14 16:48:29 -0300

    Aula03

---
+++ Aula03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula03;
+//Constantes
+
+public class Program
+{
+    public static void Main()
+    {
+
+        const string name = "Ycro";
+       // string name = "Ycro";
+
+        Console.WriteLine("O nome do individuo é: " + name);
+
+
+        dynamic idade = 20;
+
+        idade = "Saiko";
+
+        Console.WriteLine("A idade do individuo é: " + idade);
+
+        //var e dynamic são genuinamente interessantes.
+    }
+}
+
+//Em resumo, constantes são valores fixos que não mudam
